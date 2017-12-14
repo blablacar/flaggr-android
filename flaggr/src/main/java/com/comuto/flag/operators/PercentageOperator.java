e69ec92c778932c2a9bff159325177409c48b471
Nazim Benbourahla <benbourahla.nazim@gmail.com>
2017-12-14 16:50:47 +0100

    Fix issue on Percentage Operator when there no user registration date (#8)

--- flaggr/src/main/java/com/comuto/flag/operators/PercentageOperator.java
+++ flaggr/src/main/java/com/comuto/flag/operators/PercentageOperator.java
@@ -4,6 +4,8 @@ import android.support.annotation.Nullable;
 import com.comuto.flag.model.Flag;
 import org.apache.commons.lang3.StringUtils;
 
+import static com.comuto.flag.model.Flag.FlagResultStatus.DISABLED;
+
 /**
  * An operator that applied to toggle to
  * a percentage of users
@@ -23,11 +25,17 @@ public class PercentageOperator extends Operator<String> {
     @Override
     public Flag.FlagResultStatus appliesTo(@Nullable String value) throws NumberFormatException {
         if (StringUtils.isEmpty(value)) {
-            return Flag.FlagResultStatus.DISABLED;
+            return DISABLED;
         }
         final int val = Integer.parseInt(value);
+
+        //If there is no registration time
+        if (val == 0) {
+            return DISABLED;
+        }
+
         final int asPercentage = val % MAX_PERCENTAGE;
 
-        return asPercentage >= shift && asPercentage < (percentage + shift) ? Flag.FlagResultStatus.ENABLED : Flag.FlagResultStatus.DISABLED;
+        return asPercentage >= shift && asPercentage < (percentage + shift) ? Flag.FlagResultStatus.ENABLED : DISABLED;
     }
 }
